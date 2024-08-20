6cf492dbb30eea037ee15500cdce0c2d732d6bcd
Cartoonydesu <toon.pi45@gmail.com>
2024-08-20 13:49:47 +0700

    PATCH updateSkillDescription finished:

--- skill/patch_handler.go
+++ skill/patch_handler.go
@@ -8,7 +8,7 @@ import (
 
 func (h *Handler) updateSkillName(c *gin.Context) {
 	type name struct {
-		Name string
+		Name string `json:"name"`
 	}
 	p := c.Param("key")
 	var n name
@@ -32,5 +32,33 @@ func (h *Handler) updateSkillName(c *gin.Context) {
 		response.BadRequest(c, "error", "Skill not found")
 		return
 	}
-	response.Success(c, "succes", s)
+	response.Success(c, "success", s)
+}
+
+func (h *Handler) updateSkillDescription(c *gin.Context) {
+	type desc struct  {
+		Desc string `json:"description"`
+	}
+	p := c.Param("key")
+	var d desc
+	if err := c.BindJSON(&d); err != nil {
+		response.BadRequest(c, "error", "Can not extract data from json")
+		return
+	}
+	stmt, err := h.Db.Prepare("UPDATE skill SET description = $1 WHERE key = $2;")
+	if err != nil {
+		response.BadRequest(c, "error", "Statement error")
+		return
+	}
+	defer stmt.Close()
+	if _, err := stmt.Exec(d.Desc, p); err != nil {
+		response.BadRequest(c, "error", "Not be able to update skill description")
+		return
+	}
+	s, err := h.getSkillByKey(p)
+	if err != nil {
+		response.InternalServerErr(c, "error", "Skill not found")
+		return
+	}
+	response.Success(c, "success", s)	
 }
--- skill/router.go
+++ skill/router.go
@@ -8,4 +8,5 @@ func SkillRouter(r *gin.Engine, h *Handler) {
 	r.POST("/api/v1/skills", h.createSkill)
 	r.PUT("/api/v1/skills/:key", h.updateSkill)
 	r.PATCH("/api/v1/skills/:key/action/name", h.updateSkillName)
+	r.PATCH("/api/v1/skills/:key/action/description", h.updateSkillDescription)
 }
