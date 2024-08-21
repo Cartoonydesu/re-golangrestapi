ddfdd113c3a980448b3c201367a437c198d7357b
Cartoonydesu <toon.pi45@gmail.com>
2024-08-21 13:39:29 +0700

    test update description

--- skill/patch_handler_test.go
+++ skill/patch_handler_test.go
@@ -13,19 +13,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPatchSkill(t *testing.T) {
+func TestPatchSkillName(t *testing.T) {
 	db := database.NewPostgres()
 	defer db.Close()
 	h := Handler{Db: db}
 	r := gin.Default()
 	r.POST("/api/v1/skills", h.createSkill)
 	r.PUT("/api/v1/skills/:key/action/name", h.updateSkillName)
-	r.PUT("/api/v1/skills/:key/action/description", h.updateSkillDescription)
-	r.PUT("/api/v1/skills/:key/action/logo", h.updateSkillLogo)
-	r.PUT("/api/v1/skills/:key/action/tags", h.updateSkillTags)
+	// r.PUT("/api/v1/skills/:key/action/logo", h.updateSkillLogo)
+	// r.PUT("/api/v1/skills/:key/action/tags", h.updateSkillTags)
 
 	new := Skill{
-		Key:         "testUpdateAction",
+		Key:         "testUpdateName",
 		Name:        "Test",
 		Description: "test",
 		Logo:        "test",
@@ -48,7 +47,7 @@ func TestPatchSkill(t *testing.T) {
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 
-	t.Run("update unexisted skill", func(t *testing.T) {
+	t.Run("update skill name to unexisted skill", func(t *testing.T) {
 		name := name{
 			Name: "updateName",
 		}
@@ -59,12 +58,69 @@ func TestPatchSkill(t *testing.T) {
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	})
 
-	t.Run("update skill with empty skill name", func(t *testing.T) {
+	t.Run("update skill name with empty skill name", func(t *testing.T) {
 		name := name{
 			Name: "",
 		}
 		jsonValue, _ := json.Marshal(name)
 		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/skills/%v/action/name", new.Key), bytes.NewBuffer(jsonValue))
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+}
+
+func TestPatchSkillDescription(t *testing.T) {
+	db := database.NewPostgres()
+	defer db.Close()
+	h := Handler{Db: db}
+	r := gin.Default()
+	r.POST("/api/v1/skills", h.createSkill)
+	r.PUT("/api/v1/skills/:key/action/description", h.updateSkillDescription)
+	// r.PUT("/api/v1/skills/:key/action/logo", h.updateSkillLogo)
+	// r.PUT("/api/v1/skills/:key/action/tags", h.updateSkillTags)
+
+	new := Skill{
+		Key:         "testUpdateDescription",
+		Name:        "Test",
+		Description: "test",
+		Logo:        "test",
+		Tags:        []string{"test"},
+	}
+	jsonValue, _ := json.Marshal(new)
+	req, _ := http.NewRequest("POST", "/api/v1/skills", bytes.NewBuffer(jsonValue))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	t.Run("update skill description", func(t *testing.T) {
+		desc := desc{
+			Desc: "updateDesc",
+		}
+		jsonValue, _ := json.Marshal(desc)
+		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/skills/%v/action/description", new.Key), bytes.NewBuffer(jsonValue))
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+
+	t.Run("update skill description to unexisted skill", func(t *testing.T) {
+		desc := desc{
+			Desc: "updateDesc",
+		}
+		jsonValue, _ := json.Marshal(desc)
+		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/skills/%v/action/description", "unexistedkey"), bytes.NewBuffer(jsonValue))
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("update skill description with empty skill description", func(t *testing.T) {
+		desc := desc{
+			Desc: "",
+		}
+		jsonValue, _ := json.Marshal(desc)
+		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/skills/%v/action/description", new.Key), bytes.NewBuffer(jsonValue))
 		w := httptest.NewRecorder()
 		r.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusBadRequest, w.Code)
