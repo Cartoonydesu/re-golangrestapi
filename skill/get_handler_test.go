3e3301c437b785bb3aa72ddf9414bc31d07730be
Cartoonydesu <toon.pi45@gmail.com>
2024-08-23 17:13:47 +0700

    get skill by id but unfinished

--- skill/get_handler_test.go
+++ skill/get_handler_test.go
@@ -14,81 +14,108 @@ import (
 )
 
 func TestGetAllSkills(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	router := gin.Default()
-	w := httptest.NewRecorder()
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		log.Panic(err)
-	}
-	defer db.Close()
-	rows := sqlmock.NewRows([]string{"key", "name", "description", "logo", "tags"}).
-		AddRow("skill1", "Skill One", "Description for skill one", "logo1.png", pq.Array([]string{"tag1", "tag2"})).
-		AddRow("skill2", "Skill Two", "Description for skill two", "logo2.png", pq.Array([]string{"tag3", "tag4"}))
-	mock.ExpectQuery("SELECT key, name, description, logo, tags FROM skill;").WillReturnRows(rows)
-	h := &Handler{Db: db}
-	path := "/api/v1/skills"
-	router.GET(path, h.getAllSkills)
-	req, _ := http.NewRequest("GET", path, nil)
-	router.ServeHTTP(w, req)
+	t.Run("get all skill", func(t *testing.T) {
+		router := gin.Default()
+		w := httptest.NewRecorder()
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			log.Panic(err)
+		}
+		defer db.Close()
+		rows := sqlmock.NewRows([]string{"key", "name", "description", "logo", "tags"}).
+			AddRow("skill1", "Skill One", "Description for skill one", "logo1.png", pq.Array([]string{"tag1", "tag2"})).
+			AddRow("skill2", "Skill Two", "Description for skill two", "logo2.png", pq.Array([]string{"tag3", "tag4"}))
+		mock.ExpectQuery("SELECT key, name, description, logo, tags FROM skill;").WillReturnRows(rows)
+		h := &Handler{Db: db}
+		path := "/api/v1/skills"
+		router.GET(path, h.getAllSkills)
+		req, _ := http.NewRequest("GET", path, nil)
+		router.ServeHTTP(w, req)
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	expectedBody := `{"status":"success","data":[{"key":"skill1","name":"Skill One","description":"Description for skill one","logo":"logo1.png","tags":["tag1","tag2"]},{"key":"skill2","name":"Skill Two","description":"Description for skill two","logo":"logo2.png","tags":["tag3","tag4"]}]}`
-	assert.JSONEq(t, expectedBody, w.Body.String())
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
+		assert.Equal(t, http.StatusOK, w.Code)
+		expectedBody := `{"status":"success","data":[{"key":"skill1","name":"Skill One","description":"Description for skill one","logo":"logo1.png","tags":["tag1","tag2"]},{"key":"skill2","name":"Skill Two","description":"Description for skill two","logo":"logo2.png","tags":["tag3","tag4"]}]}`
+		assert.JSONEq(t, expectedBody, w.Body.String())
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Error(err)
+		}
+	})
+	t.Run("get skill query error", func(t *testing.T) {
+		router := gin.Default()
+		w := httptest.NewRecorder()
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			log.Panic(err)
+		}
+		defer db.Close()
+		mock.ExpectQuery("SELECT key, name, description, logo, tags FROM skill;").
+			WillReturnError(fmt.Errorf("query failed"))
+		h := &Handler{Db: db}
+		path := "/api/v1/skills"
+		router.GET(path, h.getAllSkills)
+		req, _ := http.NewRequest("GET", path, nil)
+		router.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		expectedBody := `{"status":"error","message":"Can not get all skills"}`
+		assert.JSONEq(t, expectedBody, w.Body.String())
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Error(err)
+		}
+	})
+	t.Run("get all skill fail", func(t *testing.T) {
+		router := gin.Default()
+		w := httptest.NewRecorder()
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			log.Panic(err)
+		}
+		defer db.Close()
+		rows := sqlmock.NewRows([]string{"name", "description", "logo", "tags"}).
+			AddRow("Skill One", "Description for skill one", "logo1.png", pq.Array([]string{"tag1", "tag2"})).
+			AddRow("Skill Two", "Description for skill two", "logo2.png", pq.Array([]string{"tag3", "tag4"}))
+		mock.ExpectQuery("SELECT key, name, description, logo, tags FROM skill;").WillReturnRows(rows)
+		h := &Handler{Db: db}
+		path := "/api/v1/skills"
+		router.GET(path, h.getAllSkills)
+		req, _ := http.NewRequest("GET", path, nil)
+		router.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		expectedBody := `{"status":"error", "message": "Can not get all skills"}`
+		assert.JSONEq(t, expectedBody, w.Body.String())
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Error(err)
+		}
+	})
 }
 
-func TestGetAllSkillsQueryError(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	router := gin.Default()
-	w := httptest.NewRecorder()
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		log.Panic(err)
-	}
-	defer db.Close()
-	mock.ExpectQuery("SELECT key, name, description, logo, tags FROM skill;").
-		WillReturnError(fmt.Errorf("query failed"))
-	h := &Handler{Db: db}
-	path := "/api/v1/skills"
-	router.GET(path, h.getAllSkills)
-	req, _ := http.NewRequest("GET", path, nil)
-	router.ServeHTTP(w, req)
+func TestGetSkillById(t *testing.T) {
+    t.Run("get skill by id", func(t *testing.T) {
+        router := gin.Default()
+        w := httptest.NewRecorder()
+        db, mock, err := sqlmock.New()
+        if err != nil {
+            log.Panic(err)
+        }
+        defer db.Close()
 
-	assert.Equal(t, http.StatusBadRequest, w.Code)
-	expectedBody := `{"status":"error","message":"Can not get all skills"}`
-	assert.JSONEq(t, expectedBody, w.Body.String())
+        rows := sqlmock.NewRows([]string{"key", "name", "description", "logo", "tags"}).
+            AddRow("skill1", "Skill One", "Description for skill one", "logo1.png", pq.Array([]string{"tag1", "tag2"}))
+        mock.ExpectQuery("SELECT key, name, description, logo, tags FROM skill WHERE key = 'skill1'").WillReturnRows(rows)
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
-}
+        h := &Handler{Db: db}
+        path := "/api/v1/skills/skill1"
+        router.GET(path, h.getSkillById)
+        req, _ := http.NewRequest("GET", path, nil)
+        router.ServeHTTP(w, req)
 
-func TestGetAllSkillsFailed(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	router := gin.Default()
-	w := httptest.NewRecorder()
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		log.Panic(err)
-	}
-	defer db.Close()
-	rows := sqlmock.NewRows([]string{"name", "description", "logo", "tags"}).
-		AddRow("Skill One", "Description for skill one", "logo1.png", pq.Array([]string{"tag1", "tag2"})).
-		AddRow("Skill Two", "Description for skill two", "logo2.png", pq.Array([]string{"tag3", "tag4"}))
-	mock.ExpectQuery("SELECT key, name, description, logo, tags FROM skill;").WillReturnRows(rows)
-	h := &Handler{Db: db}
-	path := "/api/v1/skills"
-	router.GET(path, h.getAllSkills)
-	req, _ := http.NewRequest("GET", path, nil)
-	router.ServeHTTP(w, req)
+        assert.Equal(t, http.StatusOK, w.Code)
+        expectedBody := `{"status":"success","data":{"key":"skill1","name":"Skill One","description":"Description for skill one","logo":"logo1.png","tags":["tag1","tag2"]}}`
+        assert.JSONEq(t, expectedBody, w.Body.String())
 
-	assert.Equal(t, http.StatusBadRequest, w.Code)
-	expectedBody := `{"status":"error", "message": "Can not get all skills"}`
-	assert.JSONEq(t, expectedBody, w.Body.String())
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Error(err)
-	}
+        if err := mock.ExpectationsWereMet(); err != nil {
+            t.Error(err)
+        }
+    })
 }
